1b729c686b590164026af9218326bb7a4a36fc03
i960107 <i960107@naver.com>
2024-04-18 18:31:04 +0900

    build: app-api 모듈 추가 및 의존성 설정
    - domain 모듈 추가. 추후 web 모듈 추가 예정
    - 다른 모듈과 달리 app-api모듈만 Kotlin 사용: kotlin의 장점인 null check, data class를 가장 잘 활용할 수 있는 계층이라고 판단. 일부만 도입.
    -추가된 의존성
    1. actuator: 개발 과정에서 env, url, bean 등 확인하기 위해
    2. web, validation
    3. kotlin 및 kotest
    4. devtools: thymeleaf 개발시 hot reload 사용위해
    5. thymeleaf: view 개발용

---
+++ app-api/build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
+}
+
+group = "com.aireview"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":domain"))
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity5")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
