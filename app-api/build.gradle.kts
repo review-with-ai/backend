e9b7b091cc8a995a5d1aa402d0cffe311643d32d
i960107 <i960107@naver.com>
2024-04-24 19:36:25 +0900

    build: 모듈 의존성 추가
    - web 모듈 삭제에 따라 기존 Web 모듈에 있던 의존성을 app 모듈로 변경

--- app-api/build.gradle.kts
+++ app-api/build.gradle.kts
@@ -26,13 +26,21 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity5")
+    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6:3.1.1.RELEASE")
+    implementation("io.sentry:sentry-spring-boot-starter-jakarta")
+    implementation("org.springframework.boot:spring-boot-starter-security")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
     testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("io.sentry:sentry-bom:7.3.0")
+    }
 }
 
 tasks.withType<KotlinCompile> {
