aeb8844a02bca7993095a844689c2dd7d0c5404f
i960107 <i960107@naver.com>
2024-04-24 19:38:04 +0900

    refactor: 불필요한 @component scan 제거
    모든 모듈의 base package이름을 com.aireview.review로 통일하였기 때문에 @ComponentScan 지정할 필요 없음

--- app-api/src/main/kotlin/com/aireview/review/AppApiApplication.kt
+++ app-api/src/main/kotlin/com/aireview/review/AppApiApplication.kt
@@ -2,10 +2,8 @@ package com.aireview.review
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan(basePackages = ["com.aireview"])
 class AppApiApplication
 
 fun main(args: Array<String>) {
