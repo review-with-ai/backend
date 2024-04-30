e33f74de1ab208c9636834a02619e868742663a7
i960107 <i960107@naver.com>
2024-04-30 19:19:12 +0900

    feat: GlobalExceptionHandler 추가
    - 로그인 과정에서 발생하는 AuthenticationException은 메시지 바디 없이 상태코드만 응답.
    - 다른 예외의 경우 ErrorResponse(메시지 혹은 필드 에러)를 바디에 담아 응답

---
+++ app-api/src/main/java/com/aireview/review/exception/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.aireview.review.exception;
+
+import com.aireview.review.model.ErrorResponse;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> entityNotFoundException(EntityNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<Void> authenticationException(AuthenticationException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> constraintViolation(MethodArgumentNotValidException ex, BindingResult bindingResult) {
+        Map<String, String> fieldErrors = new HashMap<>();
+
+        bindingResult.getFieldErrors()
+                .forEach(fieldError -> fieldErrors.put(fieldError.getField(), fieldError.getDefaultMessage()));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(fieldErrors));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> runtimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(ex.getMessage()));
+    }
+
+}
