6cb0f5ba257b8d250b05ef1e48353e53cefb652b
i960107 <i960107@naver.com>
2024-04-30 19:19:32 +0900

    test: 로그인 테스트 추가

---
+++ app-api/src/test/java/com/aireview/review/TestAppApiApplication.java
@@ -0,0 +1,8 @@
+package com.aireview.review;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+public class TestAppApiApplication {
+}
