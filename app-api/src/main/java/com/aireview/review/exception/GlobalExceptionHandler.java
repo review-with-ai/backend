de39648a7b2f1522f3abf1ada61e06c75c6a67d8
i960107 <i960107@naver.com>
2024-05-13 00:23:11 +0900

    feat: RuntimeException에 대한 exception handler 추가
    - runtime exception 발생시 stacktrace가 응답으로 전달되는 것을 방지하기 위해서 exceptionHandler추가. 에러 메시지만 전달.

--- app-api/src/main/java/com/aireview/review/exception/GlobalExceptionHandler.java
+++ app-api/src/main/java/com/aireview/review/exception/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpServerErrorException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -42,4 +43,9 @@ public class GlobalExceptionHandler {
                 .body(new ErrorResponse(ex.getMessage()));
     }
 
+    @ExceptionHandler(HttpServerErrorException.InternalServerError.class)
+    public ResponseEntity<ErrorResponse> internalServerError(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(ex.getMessage()));
+    }
 }
