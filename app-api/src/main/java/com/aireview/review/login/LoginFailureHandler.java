b045c5576bc80e58f020c4a39de80f823c11c565
i960107 <i960107@naver.com>
2024-05-19 22:28:10 +0900

    feat: 로그인 실패시 원인을 표현하는 메시지 body에 담아서 반환

--- app-api/src/main/java/com/aireview/review/login/LoginFailureHandler.java
+++ app-api/src/main/java/com/aireview/review/login/LoginFailureHandler.java
@@ -1,8 +1,11 @@
 package com.aireview.review.login;
 
+import com.aireview.review.model.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -10,9 +13,15 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class LoginFailureHandler implements AuthenticationFailureHandler {
+    private final ObjectMapper objectMapper;
+
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
+        response.getWriter().write(
+                objectMapper.writeValueAsString(errorResponse));
     }
 }
