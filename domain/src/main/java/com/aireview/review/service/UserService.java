fdcf47eb4a9d5bef6324f7f186a6e63236b2e626
i960107 <i960107@naver.com>
2024-05-22 01:18:45 +0900

    feat: user service 도메인 모듈로 이동
    기존 user service는 spring security에서 제공하는 d/pw 로그인 기능을 위해 UserDetailsService를 구현하였음. UserDetailsService는 app-api 모듈에 그대로 둠. filter와 관련된 로직이기 때문

--- domain/src/main/java/com/aireview/review/service/UserService.java
+++ domain/src/main/java/com/aireview/review/service/UserService.java
@@ -3,12 +3,8 @@ package com.aireview.review.service;
 import com.aireview.review.domain.user.OAuthProvider;
 import com.aireview.review.domain.user.User;
 import com.aireview.review.domain.user.UserRepository;
-import com.aireview.review.login.CustomUserDetails;
+import com.aireview.review.exception.validation.DuplicateEmailException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,28 +13,20 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class UserService implements UserDetailsService {
-
+public class UserService {
     private final UserRepository userRepository;
-
-    private final PasswordEncoder passwordEncoder;
 
     public Optional<User> findUser(Long id) {
         return userRepository.findById(id);
     }
 
-    @Transactional(readOnly = true)
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO: 4/30/24 username 중복되는 경우 에러 처리 필요 
-        User user = userRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("user not found with username " + username));
-        return new CustomUserDetails(user);
-    }
-
     public User join(String email, String password, String name, String nickname) {
-        // TODO: 4/29/24 중복 체크 필요 
-        User user = User.of(name, nickname, email, passwordEncoder.encode(password));
+        userRepository.findByEmail(email)
+                .ifPresent(user -> {
+                    throw new DuplicateEmailException();
+                });
+        // TODO: 5/22/24 password encode 모듈 정리 필요
+        User user = User.of(name, nickname, email, password);
         return userRepository.save(user);
     }
 
