f3caa7ddbe1c87a19e030ea87928bbf6de6cbc64
i960107 <i960107@naver.com>
2024-06-15 19:12:34 +0900

    feat: 정기결제 배치 테스트용 핸들러 메서드 추가

--- app-api/src/main/java/com/aireview/review/controller/SubscriptionController.java
+++ app-api/src/main/java/com/aireview/review/controller/SubscriptionController.java
@@ -8,10 +8,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/subscription")
@@ -51,4 +48,13 @@ public class SubscriptionController {
     ) {
         subscriptionPaymentService.deleteSavedPayRequest(tempOrderId);
     }
+
+    @GetMapping("/renew")
+    public void renewSubscription(
+            @AuthenticationPrincipal CustomAuthenticatedPrincipal authenticatedPrincipal,
+            @RequestParam(value = "subscriptionId") Long subscriptionId
+    ) {
+        subscriptionPaymentService.recurringPay(authenticatedPrincipal.id(), subscriptionId);
+    }
+
 }
