7682ea468c888b765e237fe4142feebb18637902
i960107 <i960107@naver.com>
2024-06-19 13:30:10 +0900

    feat: querydsl jpafactory추가

--- domain/src/main/java/com/aireview/review/config/JpaConfiguration.java
+++ domain/src/main/java/com/aireview/review/config/JpaConfiguration.java
@@ -1,6 +1,10 @@
 package com.aireview.review.config;
 
 import com.aireview.review.common.Authenticated;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
@@ -17,6 +21,9 @@ import java.util.Optional;
         basePackages = "com.aireview.review.domains",
         repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
 public class JpaConfiguration implements AuditorAware<Long> {
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public Optional<Long> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -25,5 +32,10 @@ public class JpaConfiguration implements AuditorAware<Long> {
         }
         Authenticated authenticated = (Authenticated) authentication.getPrincipal();
         return Optional.of(authenticated.getUserId());
+    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
     }
 }
